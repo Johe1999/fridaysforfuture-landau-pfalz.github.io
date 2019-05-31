a8c6af08f7b7a7d49752dfe01232aa5bfe338be0
Christophe Cauet <ccauet@point-8.de>
2019-05-31 21:12:31 +0200

    Implements API request to catch next strike date and place

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://fff-api.dmho.de/v1/scrape/groups"
+        url: "https://fff-api.dmho.de/v1/scrape/list2"
     }).then(function(data) {
-       $('#next-strike-ld').prepend(data);
+        var msg = '<a href="https://fridaysforfuture.de/streiktermine/">Kein Streik geplant</a>';
+        for (var entry of data["list"]) {
+            if (entry["city"] == "Landau") {
+              msg = entry["time"] + " (" + entry["place"] + ")";
+            }
+        }
+       $('#next-strike-ld').prepend(msg);
     });
 });
